b4b828bdf27db6257344c7244884b03c56cb3430
vunh <vunhpd1031@gmail.com>
2018-08-06 14:21:45 +0700

    fix layout

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,46 +7,61 @@ $(document).ready(function () {
         $("html").removeClass("hidden-scroll");
     });
 
-    // Cache selectors
+    // scroll landding page
     var lastId,
-        topMenu = $(".nav-list"),
-        topMenuHeight = topMenu.outerHeight() + 0,
+        topMenu = $("#myNavbar"),
+        topMenuHeight = $('#header').outerHeight(),
         // All list items
         menuItems = topMenu.find("a"),
         // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function (id) {
-            var item = $(this).attr("href");
+        scrollItems = menuItems.map(function () {
+            var item = $($(this).attr("href"));
             if (item.length) { return item; }
         });
-    var heightHeader = $('#header').height();
+    
+
     // Bind click handler to menu items
     // so we can get a fancy scroll animation
     menuItems.click(function (e) {
         var href = $(this).attr("href"),
-            offsetTop = href === "#" ? 0 : $(href).offset().top + heightHeader;
+            offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
         $('html, body').stop().animate({
             scrollTop: offsetTop
-        }, 500);
+        }, 300);
         e.preventDefault();
     });
+
     // Bind to scroll
     $(window).scroll(function () {
-        var windscroll = $(window).scrollTop();
-        // console.log($(window).scrollTop());
-        let heightHeaders = $("#header").height();
-        if (windscroll >= heightHeaders) {
-            $('.wrapper section').each(function (i) {
-                if ($(this).position().top <= windscroll + $('#myNavbar').height()) {
-                    $('.item-nav.active-menu').removeClass('active-menu');
-                    $('.item-nav').eq(i).addClass('active-menu');
-                }
-            });
-        } else {
-            $('.item-nav.active-menu').removeClass('active-menu');
-            $('.item-nav:first').removeClass('active-menu');
-        }
-    }).scroll();
+        // Get container scroll position
+        var banner = $('.banner-container').height();
+        var scroll = $(window).scrollTop();
 
+        if (scroll >= banner) {
+            $('#header').addClass('fix-top');
+        } else {
+            $('#header').removeClass('fix-top');
+        }
+
+        var fromTop = $(this).scrollTop() + topMenuHeight;
+
+        // Get id of current scroll item
+        var cur = scrollItems.map(function () {
+            if ($(this).offset().top < fromTop)
+                return this;
+        });
+        // Get the id of the current element
+        cur = cur[cur.length - 1];
+        var id = cur && cur.length ? cur[0].id : "";
+
+        if (lastId !== id) {
+            lastId = id;
+            // Set/remove active class
+            menuItems
+                .parent().removeClass("active-menu")
+                .end().filter("[href='#" + id + "']").parent().addClass("active-menu");
+        }
+    });
 
     // responsive menu
     $(".navbar-toggle").click(function () {
@@ -64,9 +79,10 @@ $(document).ready(function () {
             }
         });
     });
+    
     $('.slider').slick({
         infinite: true,
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 3
     });
     $('.slider-1').slick({
@@ -79,4 +95,10 @@ $(document).ready(function () {
         slidesToShow: 4,
         slidesToScroll: 1
     });
+    $(".custom-color").percircle({ progressBarColor: "#00a99d", percent: 75 });
+    $(".custom-color-1").percircle({ progressBarColor: "#00a99d", percent: 92 });
+    $(".custom-color-2").percircle({ progressBarColor: "#00a99d", percent: 68 });
+    $(".custom-color-3").percircle({ progressBarColor: "#00a99d", percent: 100 });
+    $(".custom-color-4").percircle({ progressBarColor: "#00a99d", percent: 83 });
+    $(".custom-color-5").percircle({ progressBarColor: "#00a99d", percent: 50 });
 });
